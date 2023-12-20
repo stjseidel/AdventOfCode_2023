54a70694b160c896f964a53752a9fe0c8f0ab7db
Stefan Seidel <stjseidel@gmail.com>
2023-12-20 17:50:40 +0100

    20 part 1 solved but simple2 depends on simple1 not being run

--- 20.py
+++ 20.py
@@ -8,7 +8,6 @@ Created on Mon Dec  4 06:32:12 2023
 from aoc_class import AOC
 from timeit import default_timer as timer
 from collections import defaultdict
-from math import prod
 
 class Signal():
     def __init__(self, sender, receiver, message):
@@ -18,7 +17,6 @@ class Signal():
         
     def __repr__(self):
         return f'{self.sender} -{self.message}-> {self.receiver}'
-        # return f'From: <{self.sender}>; To: <{self.receiver}>; message: <{self.message}>'
         
 class Modules():
     message_stack = []
@@ -30,15 +28,11 @@ class Modules():
     def push_button(self, message):
         self.send('broadcaster', Signal('button', 'broadcaster', message))
     
-    def reset_counts(self):
-        self.__class__.highs = 0
-        self.__class__.lows = 0
-        # self.__class__.signals_sent = []
                 
     
     def get_result(self):
-        lows = self.__class__.signals_sent.count(0)
-        highs = len(self.__class__.signals_sent) - lows
+        lows = Modules.signals_sent.count(0)
+        highs = len(Modules.signals_sent) - lows
         result = lows * highs
         print(f'Number of signals sent: highs: {highs}; lows: {lows}. Product: {result}.')
         return result
@@ -49,62 +43,51 @@ class Modules():
         receiver = [rec.replace(' ', '') for rec in receiver]
         for rec in receiver:
             output = Signal(signal.sender, rec, signal.message)
-            # if rec not in self.__class__.module_dict.keys():
-            #     print(f'warning, <{rec}> not in receiving modules: ', output)
-            self.__class__.message_stack.append(output)
-            # print(f'SENDING: added Signal to stack. new height: {len(Modules.message_stack)}. {output}')
-            # if rec in self.module_dict.keys():
-            # self.__class__.signals_sent.append(output.message)
-        pass
+            Modules.message_stack.append(output)
     
     def init_modules(self):
-        for module in self.__class__.module_dict.values():
-            
+        for module in Modules.module_dict.values():
             receiver = module.receiver
             if not type(receiver) == list:
                 receiver = [receiver]
             receiver = [rec.replace(' ', '') for rec in receiver]
-            print(receiver)
             for rec in receiver:
-                if not rec == 'output':
-                    self.__class__.module_dict[rec].receive(Signal(module.name, rec, module.state))
-                # signal = Signal(module.name, module.receiver, module.state)
-                # print(signal)
-                # module.send(module.receiver, signal)
-        self.__class__.message_stack = []
+                if rec in Modules.module_dict.keys():
+                    Modules.module_dict[rec].receive(Signal(module.name, rec, module.state))
+        Modules.message_stack = []
         for module in self.conjunctions:
             self.module_dict[module.name].senders = {sender:0 for sender in module.senders.keys()}
         for name, module in self.module_dict.items():
             self.module_dict[name].state = 0
-        # self.get_result()
-        self.reset_counts()
-        # self.get_result()
+        Modules.signals_sent = []
         
     def process_stack(self):
-        # while len(Modules.message_stack) > 0:
-        while self.__class__.processed < len(self.__class__.message_stack):
-            # print([f'[{mod.name}:{mod.state}]' for mod in self.__class__.module_dict.values()])
-            signal = self.__class__.message_stack[self.__class__.processed]
-            self.__class__.processed += 1
-            # current_state = self.__class__.module_dict[signal.sender].state
-            # signal = Signal(signal.sender, signal.receiver, current_state)  # to get the current state
-            self.__class__.signals_sent.append(signal.message)
-            # print(current_state, signal)
-            if signal.receiver in self.__class__.module_dict.keys():
-                state_in = self.__class__.module_dict[signal.receiver].state
-                # Modules.message_stack.pop(0)
-                self.__class__.module_dict[signal.receiver].state = self.__class__.module_dict[signal.receiver].receive(signal)
-                state_out = self.__class__.module_dict[signal.receiver].state
-                # print('state out:', self.__class__.module_dict[signal.receiver].state, f'. {signal.receiver}: [{state_in}->{state_out}]')
-                # print(f'{signal}. {signal.receiver}: [{state_in}->{state_out}]')
-            # print(f'{self.processed} Send [{signal}] Stackheight: {len(Modules.message_stack)}. Processed: {self.__class__.processed}')
-            # else:
-            #     print('OUTPUT signal:', signal)
-            # pass
-            # self.get_result()
-        # print('Message_stack is exhausted.')
-        self.__class__.message_stack = []
-        self.__class__.processed = 0
+        while Modules.processed < len(Modules.message_stack):
+            signal = Modules.message_stack[Modules.processed]
+            Modules.processed += 1
+            Modules.signals_sent.append(signal.message)
+            if not signal.receiver in Modules.module_dict.keys():
+                Modules.module_dict[signal.receiver] = Broadcaster(signal.receiver, [])
+            # state_in = Modules.module_dict[signal.receiver].state
+            Modules.module_dict[signal.receiver].receive(signal)
+            # state_out = Modules.module_dict[signal.receiver].state
+            # print(f'{signal}. {signal.receiver}: [{state_in}->{state_out}]')
+        Modules.message_stack = []
+        Modules.processed = 0
+        
+    def process_stack2(self):
+        while Modules.processed < len(Modules.message_stack):
+            signal = Modules.message_stack[Modules.processed]
+            Modules.processed += 1
+            Modules.signals_sent.append(signal.message)
+            if not signal.receiver in Modules.module_dict.keys():
+                Modules.module_dict[signal.receiver] = Broadcaster(signal.receiver, [])
+            Modules.module_dict[signal.receiver].receive(signal)
+            if signal.receiver == 'rx' and signal.message == 0:
+                return True
+        Modules.message_stack = []
+        Modules.processed = 0
+        return False
         
 
 class FlipFlop(Modules):
@@ -115,10 +98,9 @@ class FlipFlop(Modules):
         
     def receive(self, signal):
         if signal.message == 1:
-            return self.state
+            return None
         self.state = 1 - self.state
         self.send(self.receiver, Signal(self.name, self.receiver, self.state))
-        return self.state
         
 class Conjunction(Modules):
     def __init__(self, name, receiver):
@@ -134,7 +116,6 @@ class Conjunction(Modules):
         else:
             self.state = 1
         self.send(self.receiver, Signal(self.name, self.receiver, self.state))
-        return self.state
         
 class Broadcaster(Modules):
     def __init__(self, name, receiver):
@@ -145,7 +126,6 @@ class Broadcaster(Modules):
     def receive(self, signal):
         self.state = signal.message
         self.send(self.receiver, Signal(self.name, self.receiver, self.state))
-        return self.state
         
         
         
@@ -171,37 +151,41 @@ class Today(AOC):
             else:
                 print('warning, unknown input:', line)
         self.modules.init_modules()
-        
         return lines
     
-    def part1(self):
+    def part1(self, run=1):
         self.modules = Modules()
         _ = self.parse_lines()
-        for i in range(1000):
-            # self.modules.message_stack = []
-            # self.modules.reset_counts()
-            # for module in self.modules.conjunctions:
-            #     print(module, module.name, module.state, module.senders)
-            # self.modules.message_stack = []
-            # self.self.__class__.processed = 0
-            # print(len(self.modules.message_stack))
-            # for name, module in self.modules.module_dict.items():
-                # print(name, module.receiver, module.state)
-                # if isinstance(module, Conjunction):
-                #     print(module.senders)
+        self.modules.init_modules()
+        
+        for i in range(run):
             self.modules.push_button(0)
             self.modules.process_stack()
         self.result1 = self.modules.get_result()
-        
-        
+        del self.modules
         self.time1 = timer()
         return self.result1
-                
-    def part2(self):
-        lines = self.parse_lines()
-        self.result2 = 'TODO'
+        
+        
+    def part2(self, run=1):
+        self.modules = Modules()
+        self.modules.init_modules()
+        _ = self.parse_lines()
+        finished = False
+        pressed = 0
+        for i in range(run):
+            pressed += 1
+            self.modules.push_button(0)
+            finished = self.modules.process_stack2()
+            if finished:
+                print('runs:', pressed)
+                break
+        
+        self.result2 = pressed
+        
         self.time2 = timer()
         return self.result2
+                
         
     def print_final(self):
         print(f'Part 1 result is: {self.result1}. (time: {round(self.time1 - self.beginning_of_time, 2)})')
@@ -215,35 +199,36 @@ if __name__ == '__main__':
 
 # simple part 1
     today.set_lines(simple=True)
-    today.part1()
+    today.part1(run=1000)
     print(f'Part 1 <SIMPLE> result is: {today.result1}')
     
 # simple part 1
-    # today.set_lines(simple=True)
     today.lines = today.read_lines(file_path='20_simple2.txt')  
-    today.part1()
+    today.part1(run=1000)
     print(f'Part 1 <SIMPLE> result is: {today.result1}')
     
     
     
+# hard part 1
+    today.set_lines(simple=False)
+    today.part1(run=1000)
+    print(f'Part 1 <HARD> result is: {today.result1}')
+    today.stop()
+
+
 # =============================================================================
-# # hard part 1
-#     today.set_lines(simple=False)
-#     today.part1()
-#     print(f'Part 1 <HARD> result is: {today.result1}')
-#     today.stop()
-# 
-# 
 # # simple part 2
 #     today.set_lines(simple=True) 
 #     today.part2()
 #     print(f'Part 2 <SIMPLE> result is: {today.result2}')
-# 
+# =============================================================================
+
+# =============================================================================
 # # hard part 2
 #     today.set_lines(simple=False)
-#     today.part2()
+#     today.part2(1000000)
 #     print(f'Part 2 <HARD> result is: {today.result2}')
 #     today.stop()
 #     today.print_final()
-# 
 # =============================================================================
+

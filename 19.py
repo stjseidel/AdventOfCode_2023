a348e278e41382fb6d14cfa1313c49853a4cc027
Stefan Seidel <stjseidel@gmail.com>
2024-01-04 21:15:42 +0100

    unsuccessful attempt at part2 simple

--- 19.py
+++ 19.py
@@ -18,6 +18,16 @@ class Xmas_Part():
     def __repr__(self):
         return f'(x={self.x}, m={self.m}, a={self.a}, s={self.s})'
     
+class Xmas_BP():
+    def __init__(self):
+        self.x = range(1, 4001)
+        self.m = range(1, 4001)
+        self.a = range(1, 4001)
+        self.s = range(1, 4001)
+    def __repr__(self):
+        return f'(x={self.x}, m={self.m}, a={self.a}, s={self.s})'
+    
+    
 class Today(AOC):
     # def __init__(self, day):
     #     AOC.__init__(self, day)
@@ -68,6 +78,54 @@ class Today(AOC):
                         self.check_xmas_part(xmas_part, target)
                         return None
     
+    def check_blueprint(self, blueprint, rangedict):
+        rangedict
+        print(rangedict, blueprint)
+        # breakpoint()
+        if blueprint == 'R':
+            self.rejected.append(rangedict.copy())
+            print(rangedict)
+            return None
+        elif blueprint == 'A':
+            self.accepted_ranges.append(rangedict.copy())
+            print(rangedict)
+            return None
+        bp = self.blueprints[blueprint]
+        print(blueprint, bp)
+        # neg_rangedict = rangedict.copy()  # positive result gets sent to target with updated ranges
+        neg_rangedict = {char:[] for char in 'xmas'}  # positive result gets sent to target with updated ranges
+        for i, check in enumerate(bp):
+            print(check)
+            if i < len(bp)-1:
+                if ':' in check:
+                    if check.split(':')[-1] == bp[i+1]:
+                        check = bp[i+1]
+            if check in ['R', 'A'] or ':' not in check:
+                self.check_blueprint(check, neg_rangedict)
+                return None
+            evalcheck, target = check.split(':')[0], check.split(':')[1]
+            # for char in ['<', '>', '=']:
+            dimension = evalcheck[0]
+            char = evalcheck[1]
+            value = int(evalcheck.split(char)[1])
+            if char == '<':
+                positive = [range(0, value)]
+                negative = [range(value, 4001)]
+            elif char == '>':
+                positive = [range(value + 1, 4001)]
+                negative = [range(0, value + 1)]
+            elif char == '=':
+                positive = [range(value, value+1)]
+                negative = [range(0, value), range(value+1, 4001)]
+                
+            pos_rangedict = rangedict.copy()  # positive result gets sent to target with updated ranges
+            pos_rangedict[dimension].extend(negative)
+            print(dimension, negative)
+            self.check_blueprint(target, pos_rangedict)
+            
+            print(dimension, positive)
+            neg_rangedict[dimension].extend(positive)  # this stays here in this loop
+    
     def part1(self):
         self.accepted = []
         self.rejected = []
@@ -81,9 +139,42 @@ class Today(AOC):
     def part2(self):
         # lines = self.parse_lines()
         lines = self.parse_lines()
-        for bp in self.blueprints:
+        self.accepted_ranges = []
+        # breakpoint()
+        # for bp in self.blueprints:
+        for bp in ['in']:
             print(bp, self.blueprints[bp])
-        self.result2 = 'TODO'
+            self.bp_full = Xmas_BP()
+            self.check_blueprint(bp, rangedict = {char:[] for char in 'xmas'})
+            print(self.accepted_ranges)
+        # breakpoint()
+        result2 = 0
+        passing = Xmas_BP()
+        # self.accepted_ranges = list(set(self.accepted_ranges))
+        # unique_dicts = [dict(t) for t in {tuple(d.items()) for d in self.accepted_ranges}]
+
+
+        list_of_dicts = self.accepted_ranges.copy()
+        
+        def ranges_to_tuples(d):
+            return {key: [tuple(rng) for rng in value] for key, value in d.items()}
+        
+        def dict_to_tuple(d):
+            return tuple(sorted((key, tuple(sorted(value))) for key, value in ranges_to_tuples(d).items()))
+        
+        unique_dicts = [dict(t) for t in {dict_to_tuple(d) for d in list_of_dicts}]
+
+        for ranges in unique_dicts:
+            # print(ranges)
+            x_ = len(set(passing.x) - {value for r in ranges['x'] for value in r})
+            m_ = len(set(passing.m) - {value for r in ranges['m'] for value in r})
+            a_ = len(set(passing.a) - {value for r in ranges['a'] for value in r})
+            s_ = len(set(passing.s) - {value for r in ranges['s'] for value in r})
+            result = x_ * m_ * a_ * s_
+            result2 += result
+            print(result)
+        breakpoint()
+        self.result2 = result2
         self.time2 = timer()
         return self.result2
         
